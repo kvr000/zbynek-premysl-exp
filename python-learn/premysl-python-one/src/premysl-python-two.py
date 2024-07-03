21edcf21f51769218b3a5b0e277f7095c62d8b3f
Premysl Vyskovsky <kvr000+premysl@gmail.com>
2024-07-02 20:15:53 -0700

    Dungeon game

---
+++ python-learn/premysl-python-one/src/premysl-python-two.py
@@ -0,0 +1,29 @@
+print("You are in a dark room in castle dragonsnax ")
+print("In front of you are ten doors. you must choose one.")
+playerChoice = input("Choose 1, 2, 3, 4, 5, 6, 7, 8, 9 or 10...")
+if playerChoice =="1":
+    print()
+    print("You find a room full of treasure. You're rich!")
+    print("GAME OVER, YOU WIN")
+elif playerChoice == "2":
+    print("The door opens and an angry ogre hits you with his club")
+    print("GAME OVER. YOU LOSE")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
