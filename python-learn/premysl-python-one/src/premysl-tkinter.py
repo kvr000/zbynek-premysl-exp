bab094622a433f0712225d3f956c2e25b3bd0c14
Zbynek Vyskovsky <kvr000+premysl@gmail.com>
2024-12-08 20:38:09 -0800

    Bomb finder

--- python-learn/premysl-python-one/src/premysl-tkinter.py
+++ python-learn/premysl-python-one/src/premysl-tkinter.py
@@ -20,13 +20,62 @@ def create_bombfield(bombfield):
             if random.randint(1,100) < 20:
                 rowList.append(1)
             else:
-                rowList.append(1)
+                rowList.append(0)
                 squaresToClear = squaresToClear + 1
         bombfield.append(rowList)
-    printfield(bombfield)
+
 def printfield(bombfield):
     for rowList in bombfield:
         print(rowList)
+def on_click(event):
+    global score
+    global gameOver
+    global squaresToClear
+    square = event.widget
+    row = int (square.grid_info()["row"])
+    column = int(square.grid_info()["column"])
+    currentText = square.cget("text")
+    if gameOver == False:
+        if bombfield[row][column] == 1:
+            gameOver = True
+            square.config(bg = "red")
+            print("Game Over! You hit a bomb")
+            print("Your score was: ",score)
+        elif currentText == "    ":
+            square.config(bg = "brown")
+            totalBombs = 0
+            if row < 9:
+                if bombfield[row+1][column] == 1:
+                    totalBombs = totalBombs + 1
+            if row > 0:
+                if bombfield[row-1][column] == 1:
+                    totalBombs = totalBombs + 1
+            if column > 0:
+                if bombfield[row][column-1] == 1:
+                    totalBombs = totalBombs + 1
+            if column < 9 :
+                if bombfield[row][column+1] == 1:
+                    totalBombs = totalBombs + 1
+            if row > 0 and column > 0:
+                if bombfield[row-1][column-1] == 1:
+                    totalBombs = totalBombs + 1
+            if row < 9 and column > 0:
+                if bombfield[row+1][column-1] == 1:
+                    totalBombs = totalBombs + 1
+            if row > 0 and column < 9:
+                if bombfield[row-1][column+1] == 1:
+                    totalBombs = totalBombs + 1
+            if row < 9  and column < 9:
+                if bombfield[row+1][column+1] == 1:
+                    totalBombs = totalBombs + 1
+            square.config(text =" " + str(totalBombs) + " ")
+            squaresToClear = squaresToClear - 1
+            score = score +1
+            if squaresToClear == 0:
+                gameOver = True
+                print("Well done! You found all the safe squares!")
+                print("Your score was:", score)
+
 def layout_window(window):
     for rowNumber, rowList in enumerate(bombfield):
         for columnNumber, columnEntry in enumerate(rowList):
@@ -37,13 +86,6 @@ def layout_window(window):
             else:
                 square = tkinter.Label(window, text = "    ", bg = "green")
             square.grid(row = rowNumber, column = columnNumber)
+            square.bind("<Button-1>", on_click)
 
 play_bombdodger()
-
-
-
-
-
-
-
-
